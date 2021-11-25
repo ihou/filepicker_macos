cbf6cc949b86c9f03950f87dad4ecb2945fc15de
ihou <i.hou@163.com>
2021-11-25 22:02:53 +0800

    return explicit list type

--- lib/filepicker_macos.dart
+++ lib/filepicker_macos.dart
@@ -5,12 +5,12 @@ import 'package:flutter/services.dart';
 class FilepickerMacos {
   static const MethodChannel _channel = MethodChannel('filepicker_macos');
 
-  static Future<List> pickFile() async {
-    final List  list = await _channel.invokeMethod('pickFiles');
+  static Future<List<String>> pickFile() async {
+    final List list = await _channel.invokeMethod('pickFiles');
     return list.map((e) => e.toString()).toList(growable: false);
   }
 
-  static Future<List> pickDir() async {
+  static Future<List<String>> pickDir() async {
     final List list = await _channel.invokeMethod('pickDir');
     return list.map((e) => e.toString()).toList(growable: false);
   }
